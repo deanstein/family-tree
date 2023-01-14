07389fe5a7a74ad9cba4abe79144d492cb702bc3
deanstein <jdeangoldstein@gmail.com>
2023-01-14 11:10:31 -0700

    Updating to be more like website-experimental

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<main>
+  <div class="app">
+    Hello world!
+  </div>
+</main>
+
+<style>
+  :root {
+    font-family: proxima-nova, sans-serif;
+  }
+
+  :global(body) {
+    margin: 0;
+  }
+</style>
