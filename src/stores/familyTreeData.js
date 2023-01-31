650bd02b5ede063c0e51855591ed7517eeff12a9
deanstein <jdeangoldstein@gmail.com>
2023-01-30 22:32:05 -0700

    Adding early support for relationship types

--- src/stores/familyTreeData.js
+++ src/stores/familyTreeData.js
@@ -2,15 +2,65 @@ import { writable } from 'svelte/store';
 import { v4 as uuidv4 } from 'uuid';
 
 const familyTreeData = {};
+const relationshipTypes = {
+	grandparents: {
+		grandmothermaternal: 'maternal grandmother',
+		grandmotherpaternal: 'paternal grandmother',
+		grandfathermaternal: 'maternal grandfather',
+		grandfatherpaternal: 'paternal grandfather'
+	},
+	parents: {
+		mother: 'mother',
+		father: 'father',
+		motherinlaw: 'mother-in-law',
+		fatherinlaw: 'father-in-law',
+		stepmother: 'stepmother',
+		stepfather: 'stepfather'
+	},
+	siblings: {
+		sister: 'sister',
+		brother: 'brother',
+		halfsister: 'half sister',
+		halfbrother: 'half brother',
+		stepsister: 'stepsister',
+		stepbrother: 'stepbrother',
+		sisterinlaw: 'sister-in-law',
+		brotherinlaw: 'brother-in-law'
+	},
+	spouses: {
+		wife: 'wife',
+		husband: 'husband',
+		exwife: 'ex-wife',
+		exhusband: 'ex-husband',
+		girlfriend: 'girlfriend',
+		boyfriend: 'boyfriend',
+		partner: 'partner'
+	},
+	children: {
+		daughter: 'daughter',
+		son: 'son',
+		daughterinlaw: 'daughter-in-law',
+		soninlaw: 'son-in-law',
+		stepdaughter: 'stepdaughter',
+		stepson: 'stepson'
+	},
+	grandchildren: {
+		granddaughter: 'granddaughter',
+		grandson: 'grandson',
+	}
+};
 const defaultPerson = {
 	id: 0,
 	name: 'Firstname Lastname',
 	birthdate: 'unspecified',
+	greatgrandparents: [],
+	grandparents: [],
 	parents: [],
 	siblings: [],
-	activeSpouses: [],
-	exSpouses: [],
-	children: []
+	spouses: [],
+	children: [],
+	grandchildren: [],
+	greatgrandchildren: []
 };
 const activePerson = {};
 const people = [
