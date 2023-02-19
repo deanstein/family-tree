16a87093dc767e24b9883641250442a06fece054
deanstein <jdeangoldstein@gmail.com>
2023-02-19 10:59:47 -0700

    Finishing PersonNode text input so it updates the FamilyTreeData store

--- src/ui/TextInput.svelte
+++ src/ui/TextInput.svelte
@@ -1,5 +1,7 @@
 <script>
-	import uiState from '../stores/uiState';
+	import familyTreeData from '../stores/familyTreeData';
+    import uiState from '../stores/uiState';
+    import { getPersonIndexById } from '../logic/personManagement';
 
 	export let personId = undefined;
 	export let sInputValue;
@@ -14,10 +16,16 @@
 		}
 	}
 
-	function startEditText(el) {
+	const startEditText = (el) => {
 		el.focus();
 		el.select();
 	}
+
+    const onKeyDownAction = (event) => {
+        if (event.keyCode === 13) {
+            event.target.blur()
+        }
+    }
 
 	const onDoubleCLickAction = (event) => {
 		bEnabled = true;
@@ -28,8 +36,16 @@
 		}
 	};
 
-	const onBlurAction = () => {
-		uiState.update((currentValue) => {
+	const onBlurAction = (event) => {
+        const sCurrentInputValue = event.target.value;
+
+        familyTreeData.update(currentValue => {
+            let personIndex = getPersonIndexById(personId);
+            currentValue.people[personIndex].name = sCurrentInputValue;
+            return currentValue;
+        })
+
+		uiState.update(currentValue => {
 			currentValue.personIdForNodeEdit = undefined;
 			return currentValue;
 		});
@@ -38,12 +54,12 @@
 
 <div id="text-input-container" class="text-input-container">
 	<input
+        bind:value={sInputValue}
 		type="text"
-		value={sInputValue}
 		id="text-input"
 		class="text-input"
 		on:click|stopPropagation
-		on:keydown|stopPropagation
+		on:keydown|stopPropagation={onKeyDownAction}
 		on:dblclick={onDoubleCLickAction}
 		on:blur={onBlurAction}
 		disabled={!bEnabled}
