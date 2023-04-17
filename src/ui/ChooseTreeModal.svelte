43f63baa869341fb134d3fadbd617683566fb2bb
deanstein <jdeangoldstein@gmail.com>
2023-04-16 19:10:22 -0600

    Styling improvements for ChoosTreeModal

--- src/ui/ChooseTreeModal.svelte
+++ src/ui/ChooseTreeModal.svelte
@@ -7,16 +7,29 @@
 	import stylingConstants from './stylingConstants';
 
 	const chooseTreeModalContentContainerDynamicClass = css`
-		z-index: ${stylingConstants.zIndices.nPersonNodeEditZIndex};
+		z-index: ${stylingConstants.zIndices.nPersonNodeEditZIndex + 1};
 	`;
 </script>
 
 {#if $uiState.showChooseTreeModal}
 	<div
 		id="choose-tree-modal-outer-container"
-		class="{chooseTreeModalContentContainerDynamicClass} choose-tree-modal-outer-container"
+		class="choose-tree-modal-outer-container"
 	>
-		<div id="choose-tree-modal-content-container" class="choose-tree-modal-content-container" />
+		<div id="choose-tree-modal-content-container" class="{chooseTreeModalContentContainerDynamicClass} choose-tree-modal-content-container">
+			<div id="choose-tree-header" class="choose-tree-header">Welcome! Choose a family tree:</div>
+			<div id="choose-tree-options-grid" class="choose-tree-options-grid">
+				<div id="new-tree-cell-container" class="options-cell-container">
+					New Family Tree
+				</div>
+				<div id="example-tree-cell-container" class="options-cell-container">
+					Example Family Tree
+				</div>
+				<div id="load-tree-cell-container" class="options-cell-container">
+					Load Family Tree
+				</div>
+			</div>
+		</div>
 		<Overlay />
 	</div>
 {/if}
@@ -32,8 +45,25 @@
 	}
 
 	.choose-tree-modal-content-container {
-		width: 10vh;
-		height: 10vh;
+		width: 50vw;
+		height: 50vh;
 		background-color: black;
+	}
+
+	.choose-tree-header {
+		background-color: gray;
+	}
+
+	.choose-tree-options-grid {
+		display: flex;
+		height: 100%;
+	}
+
+	.options-cell-container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-basis: 33%;
+		color: white;
 	}
 </style>
