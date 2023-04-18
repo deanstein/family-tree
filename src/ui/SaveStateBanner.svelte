190ba1803c756328ce78448064505b6efd343fd6
deanstein <jdeangoldstein@gmail.com>
2023-04-17 21:23:56 -0600

    Adding initial self-dismissing notification

--- src/ui/SaveStateBanner.svelte
+++ src/ui/SaveStateBanner.svelte
@@ -1,19 +1,34 @@
 <script>
-	import { getNotificationConfigFromSaveState } from '../logic/uiManagement';
+	import { getNotificationConfigFromRepoState, setRepoState } from '../logic/uiManagement';
 	import uiState from '../stores/uiState';
 
 	import NotificationBanner from './NotificationBanner.svelte';
+	import { repoStateStrings } from './strings';
 
 	let message;
 	let color;
 
+	$: {
+		// success messages get a 5-second delay before dismissing
+		if (
+			$uiState.saveToRepoStatus === repoStateStrings.loadSuccessful ||
+			$uiState.saveToRepoStatus === repoStateStrings.saveSuccessful
+		) {
+			setTimeout(() => {
+				setRepoState(repoStateStrings.undefined);
+			}, 1500);
+		}
+	}
+
 	uiState.subscribe(() => {
-		message = getNotificationConfigFromSaveState().message;
-		color = getNotificationConfigFromSaveState().color;
+		message = getNotificationConfigFromRepoState().message;
+		color = getNotificationConfigFromRepoState().color;
 	});
 </script>
 
-<NotificationBanner {message} {color} />
+{#if $uiState.saveToRepoStatus != repoStateStrings.undefined && $uiState.saveToRepoStatus != repoStateStrings.saved && $uiState.saveToRepoStatus != undefined}
+	<NotificationBanner {message} {color} />
+{/if}
 
 <style>
 </style>
