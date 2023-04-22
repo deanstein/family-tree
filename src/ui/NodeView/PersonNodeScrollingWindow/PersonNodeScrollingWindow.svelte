795c1fb6a7368e3ff944e487d46b03f0fe420506
deanstein <jdeangoldstein@gmail.com>
2023-04-21 19:49:28 -0600

    Fixing styling issues with PersonNodeScrollingWindow

--- src/ui/NodeView/PersonNodeScrollingWindow/PersonNodeScrollingWindow.svelte
+++ src/ui/NodeView/PersonNodeScrollingWindow/PersonNodeScrollingWindow.svelte
@@ -8,28 +8,36 @@
 </script>
 
 <div
-	id="person-node-scrolling-window-container"
-	class="person-node-scrolling-window-container"
-	on:click|stopPropagation
-	on:keypress|stopPropagation
-	on:wheel={scrollHorizontal}
+	id="person-node-scrolling-window-outer-container"
+	class="person-node-scrolling-window-outer-container"
 >
-	{#each $uiState.personIdsOffScreenFiltered as sPersonId}
-		<PersonNodeForSelect {sPersonId} {sRelationshipId} />
-	{/each}
+	<div
+		id="person-node-scrolling-window-inner-container"
+		class="person-node-scrolling-window-inner-container"
+		on:click|stopPropagation
+		on:keypress|stopPropagation
+		on:wheel={scrollHorizontal}
+	>
+		{#each $uiState.personIdsOffScreenFiltered as sPersonId}
+			<PersonNodeForSelect {sPersonId} {sRelationshipId} />
+		{/each}
+	</div>
 </div>
 
 <style>
-	.person-node-scrolling-window-container {
-		display: flex;
+	.person-node-scrolling-window-outer-container {
 		position: absolute;
-		justify-content: center;
-		overflow: auto;
-		max-width: 500%; /* 5x the width of the node */
 		top: 100%;
-		gap: 10px;
+		max-width: 500%; /* 5x the width of the node */
 		z-index: 1;
 		padding: 10px;
 		background-color: gray;
+	}
+
+	.person-node-scrolling-window-inner-container {
+		display: flex;
+		overflow-x: auto;
+		overflow-y: hidden;
+		gap: 10px;
 	}
 </style>
