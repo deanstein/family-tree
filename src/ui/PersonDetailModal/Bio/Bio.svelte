0767018dbc93cb4f20457d473b90393194d9c843
deanstein <jdeangoldstein@gmail.com>
2023-04-29 23:18:33 -0600

    Splitting Bio into its own component

---
+++ src/ui/PersonDetailModal/Bio/Bio.svelte
@@ -0,0 +1,61 @@
+<script>
+    import uiState from "../../../stores/ui-state";
+
+    import Avatar from "../../NodeView/PersonNode/Avatar.svelte";
+</script>
+
+<div id="bio-content-container" class="bio-content-container">
+    <div id="bio-avatar-container" class="bio-avatar-container">
+        <Avatar />
+    </div>
+    <div id="bio-name" class="bio-name">
+        {$uiState.activePerson.name}
+    </div>
+    <div id="bio-facts" class="bio-facts">
+        <div id="bio-fact" class="bio-fact">Also known as:</div>
+        <div id="bio-fact" class="bio-fact">Gender:</div>
+        <div id="bio-fact" class="bio-fact">Date of Birth:</div>
+        <div id="bio-fact" class="bio-fact">Time of Birth:</div>
+        <div id="bio-fact" class="bio-fact">Place of Birth:</div>
+        <div id="bio-fact" class="bio-fact">Hometown:</div>
+        <div id="bio-fact" class="bio-fact">Deceased?</div>
+    </div>
+</div>
+
+<style>
+	.bio-content-container {
+		display: flex;
+		flex-direction: column;
+		flex-grow: 1;
+		justify-content: center;
+		align-items: center;
+		background-color: gainsboro;
+		padding: 1vh;
+	}
+
+	.bio-avatar-container {
+		width: 20vh;
+		height: 20vh;
+	}
+
+	.bio-name {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		padding: 10px;
+		color: white;
+		font-size: 5vh;
+	}
+
+	.bio-facts {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		width: 100%;
+		gap: 1vh;
+	}
+
+	.bio-fact {
+		background-color: darkgray;
+	}
+</style>
--- src/ui/PersonDetailModal/PersonDetailModal.svelte
+++ src/ui/PersonDetailModal/PersonDetailModal.svelte
@@ -2,21 +2,18 @@
 	import { css } from '@emotion/css';
 	import Portal from 'svelte-portal';
 
-	import familyTreeData from '../../stores/family-tree-data';
 	import uiState from '../../stores/ui-state';
 	import stylingConstants from '../styling-constants';
 	import { hidePersonDetailView } from '../../logic/ui-management';
 
-	import Avatar from '../NodeView/PersonNode/Avatar.svelte';
 	import Button from '../Button.svelte';
 	import Overlay from '../NodeView/Overlay.svelte';
 	import Timeline from './Timeline/Timeline.svelte';
+	import Bio from './Bio/Bio.svelte';
 
 	const personDetailContainerDynamicClass = css`
 		z-index: ${stylingConstants.zIndices.personDetailViewZIndex};
 	`;
-
-	const personDetailContentContainerDynamicClass = css``;
 
 	const closeButtonOnClick = () => {
 		hidePersonDetailView();
@@ -30,23 +27,7 @@
 			class="person-detail-content-container {personDetailContainerDynamicClass}"
 		>
 			<div id="person-detail-bio-container" class="person-detail-bio-container">
-				<div id="person-detail-bio-content-container" class="person-detail-bio-content-container">
-					<div id="person-detail-bio-avatar-container" class="person-detail-bio-avatar-container">
-						<Avatar />
-					</div>
-					<div id="person-detail-bio-name" class="person-detail-bio-name">
-						{$uiState.activePerson.name}
-					</div>
-					<div id="person-detail-bio-facts" class="person-detail-bio-facts">
-						<div id="person-detail-bio-fact" class="person-detail-bio-fact">Also known as:</div>
-						<div id="person-detail-bio-fact" class="person-detail-bio-fact">Gender:</div>
-						<div id="person-detail-bio-fact" class="person-detail-bio-fact">Date of Birth:</div>
-						<div id="person-detail-bio-fact" class="person-detail-bio-fact">Time of Birth:</div>
-						<div id="person-detail-bio-fact" class="person-detail-bio-fact">Place of Birth:</div>
-						<div id="person-detail-bio-fact" class="person-detail-bio-fact">Hometown:</div>
-						<div id="person-detail-bio-fact" class="person-detail-bio-fact">Deceased?</div>
-					</div>
-				</div>
+				<Bio />
 			</div>
 			<div id="person-detail-timeline-container" class="person-detail-timeline-container">
 				<div
@@ -88,42 +69,6 @@
 		display: flex;
 		flex-direction: column;
 		flex-basis: 33.333%;
-	}
-
-	.person-detail-bio-content-container {
-		display: flex;
-		flex-direction: column;
-		flex-grow: 1;
-		justify-content: center;
-		align-items: center;
-		background-color: gainsboro;
-		padding: 1vh;
-	}
-
-	.person-detail-bio-avatar-container {
-		width: 20vh;
-		height: 20vh;
-	}
-
-	.person-detail-bio-name {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		padding: 10px;
-		color: white;
-		font-size: 5vh;
-	}
-
-	.person-detail-bio-facts {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		width: 100%;
-		gap: 1vh;
-	}
-
-	.person-detail-bio-fact {
-		background-color: darkgray;
 	}
 
 	.person-detail-timeline-container {
