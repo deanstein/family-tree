ccf4971683fe053c4bb367addd826291f1111a43
deanstein <jdeangoldstein@gmail.com>
2023-12-18 19:23:58 -0700

    Fixing vite issue, switching back to static folder

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [svelte()],
+	publicDir: 'static',
+	base: '',
 	resolve: {
 		alias: {
 			$lib: path.resolve('./src/lib')
