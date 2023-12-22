89b37841e870fae8f224feca01b9c9bf8a6ff3b1
deanstein <jdeangoldstein@gmail.com>
2023-12-22 16:40:05 -0700

    Splitting MediaGallery and MediaGalleryModal

---
+++ src/lib/components/MediaGallery.svelte
@@ -0,0 +1,83 @@
+<script>
+	import tempState from '$lib/stores/temp-state';
+
+	import {
+		repoOwner,
+		dataRepoName,
+		tempPw,
+		imagePlaceholderSrc
+	} from '$lib/persistence-management';
+	import {
+		setImageEditContent,
+		setImageEditId,
+		unsetMediaGalleryActiveContent,
+		unsetMediaGalleryActiveId
+	} from '$lib/temp-management';
+
+	import Button from './Button.svelte';
+	import ImageThumbnailGroup from '$lib/components/ImageThumbnailGroup.svelte';
+	import ImageAsyncFromUrl from '$lib/components/ImageAsyncFromUrl.svelte';
+
+	const onClickEditImageButton = () => {
+		setImageEditId($tempState.mediaGalleryActiveId);
+		setImageEditContent($tempState.mediaGalleryActiveContent);
+		unsetMediaGalleryActiveId();
+		unsetMediaGalleryActiveContent();
+	};
+</script>
+
+<div class="media-gallery-content">
+	<div class="image-actions-bar">
+		<Button buttonText={'Edit Image'} onClickFunction={onClickEditImageButton} />
+	</div>
+	<div class="image-container">
+		<ImageAsyncFromUrl
+			{repoOwner}
+			repoName={dataRepoName}
+			password={tempPw}
+			imageUrl={$tempState?.mediaGalleryActiveContent?.url}
+			{imagePlaceholderSrc}
+			allowEdit={false}
+			imageFit="contain"
+		/>
+	</div>
+	<div class="media-gallery-thumbnail-container">
+		<ImageThumbnailGroup
+			showGroupTitle={false}
+			showAddButton={false}
+			imageArray={$tempState.mediaGalleryActiveContentArray}
+		/>
+	</div>
+</div>
+
+<style>
+	.media-gallery-content {
+		display: flex;
+		flex-direction: column;
+		width: -webkit-fill-available;
+		width: -moz-available;
+		height: -webkit-fill-available;
+		height: -moz-available;
+	}
+
+	.image-actions-bar {
+		position: absolute;
+		width: -webkit-fill-available;
+		padding: 0px 10px 0px 10px;
+		display: flex;
+		align-items: center;
+		justify-content: right;
+		z-index: 1;
+	}
+
+	.image-container {
+		flex-shrink: 1;
+		min-height: 0;
+	}
+
+	.media-gallery-thumbnail-container {
+		padding: 10px;
+		margin-top: 10px;
+		background-color: white;
+	}
+</style>
--- src/lib/components/MediaGalleryModal.svelte
+++ src/lib/components/MediaGalleryModal.svelte
@@ -1,31 +1,12 @@
 <script>
-	import {
-		repoOwner,
-		dataRepoName,
-		tempPw,
-		imagePlaceholderSrc
-	} from '$lib/persistence-management';
 	import tempState from '$lib/stores/temp-state';
+
+	import { unsetMediaGalleryActiveContent, unsetMediaGalleryActiveId } from '$lib/temp-management';
 
 	import stylingConstants from '$lib/components/styling-constants';
 
+	import MediaGallery from './MediaGallery.svelte';
 	import Modal from '$lib/components/Modals/Modal.svelte';
-	import ImageThumbnailGroup from '$lib/components/ImageThumbnailGroup.svelte';
-	import ImageAsyncFromUrl from '$lib/components/ImageAsyncFromUrl.svelte';
-	import {
-		setImageEditContent,
-		setImageEditId,
-		unsetMediaGalleryActiveContent,
-		unsetMediaGalleryActiveId
-	} from '$lib/temp-management';
-	import Button from './Button.svelte';
-
-	const onClickEditButton = () => {
-		setImageEditId($tempState.mediaGalleryActiveId);
-		setImageEditContent($tempState.mediaGalleryActiveContent);
-		unsetMediaGalleryActiveId();
-		unsetMediaGalleryActiveContent();
-	};
 
 	const onClickCloseButton = () => {
 		unsetMediaGalleryActiveId();
@@ -33,71 +14,28 @@
 	};
 </script>
 
-<div class="media-gallery-container">
-	<Modal
-		showModal={$tempState.mediaGalleryActiveId}
-		showCloseButton={true}
-		{onClickCloseButton}
-		width={stylingConstants.sizes.modalFullScreenWidth}
-		height={stylingConstants.sizes.modalFullScreenHeight}
-		title={'Gallery'}
-		subtitle={null}
-		zIndex={stylingConstants.zIndices.mediaGalleryZIndex}
-	>
-		<div class="media-gallery-content" slot="modal-content-slot">
-			<div class="image-actions-bar">
-				<Button buttonText={'Edit Image'} onClickFunction={onClickEditButton} />
-			</div>
-			<div class="image-container">
-				<ImageAsyncFromUrl
-					{repoOwner}
-					repoName={dataRepoName}
-					password={tempPw}
-					imageUrl={$tempState?.mediaGalleryActiveContent?.url}
-					{imagePlaceholderSrc}
-					allowEdit={false}
-					imageFit="contain"
-				/>
-			</div>
-			<div class="media-gallery-thumbnail-container">
-				<ImageThumbnailGroup
-					showGroupTitle={false}
-					showAddButton={false}
-					imageArray={$tempState.mediaGalleryActiveContentArray}
-				/>
-			</div>
-		</div>
-	</Modal>
-</div>
+<Modal
+	showModal={$tempState.mediaGalleryActiveId}
+	showCloseButton={true}
+	{onClickCloseButton}
+	width={stylingConstants.sizes.modalFullScreenWidth}
+	height={stylingConstants.sizes.modalFullScreenHeight}
+	title={'Gallery'}
+	subtitle={null}
+	zIndex={stylingConstants.zIndices.mediaGalleryZIndex}
+>
+	<div class="media-content-slot" slot="modal-content-slot">
+		<MediaGallery />
+	</div>
+</Modal>
 
 <style>
-	.media-gallery-content {
+	.media-content-slot {
 		display: flex;
 		flex-direction: column;
 		width: -webkit-fill-available;
 		width: -moz-available;
 		height: -webkit-fill-available;
 		height: -moz-available;
-	}
-
-	.image-actions-bar {
-		position: absolute;
-		width: -webkit-fill-available;
-		padding: 0px 10px 0px 10px;
-		display: flex;
-		align-items: center;
-		justify-content: right;
-		z-index: 1;
-	}
-
-	.image-container {
-		flex-shrink: 1;
-		min-height: 0;
-	}
-
-	.media-gallery-thumbnail-container {
-		padding: 10px;
-		margin-top: 10px;
-		background-color: white;
 	}
 </style>
