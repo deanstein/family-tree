a60030afb15d39a609ca45d3705b2cb2450c6eec
deanstein <jdeangoldstein@gmail.com>
2025-07-16 20:24:51 -0600

    Fixing issues with ComposeToolbar

--- src/lib/components/ComposeToolbar.svelte
+++ src/lib/components/ComposeToolbar.svelte
@@ -5,12 +5,11 @@
 	import { getNearestScrollingElement } from '$lib/utils';
 
 	import ComposeButton from './ComposeButton.svelte';
-	import stylingConstants from './styling-constants';
 
 	export let parentRef; // required to determine where to show the compose button
 	export let isEditActive = false; // when active, show done/cancel
 	export let onClickCompose; // edit, add, etc
-	export let composeButtonFaIcon = 'fa-pencil';
+	export let composeButtonFaIcon = 'fa-pencil fa-fw';
 	export let composeButtonTooltip = 'Compose';
 	export let onClickDone = undefined; // if provided, show done
 	export let doneButtonLabel = 'Done';
@@ -21,41 +20,58 @@
 	export let onClickDelete = undefined; // if provided, show delete
 	export let deleteButtonTooltip = 'Delete';
 	export let deleteButtonLabel = 'Delete';
+	export let zIndex = 1000; // override depending on the context
 
 	let parentHeight;
-	let heightFromTopPx;
+	let parentWidth;
+	let buttonWidth;
+	let distanceFromTopPx;
+	let distanceFromRightPx;
 	let composeContainerRef;
-	let composeButtonHeight;
 	let composeToolbarWrapperRef;
 
+	// determine the height and width at which to show the compose button
 	$: {
+		if (composeToolbarWrapperRef) {
+			// compose button is assumed circular, so width is same as height
+			buttonWidth = composeContainerRef.getBoundingClientRect().height;
+		}
+
 		if (parentRef) {
 			const scrollingContainer = getNearestScrollingElement(parentRef);
 			parentHeight = scrollingContainer.getBoundingClientRect().height;
-			heightFromTopPx = parentHeight * stylingConstants.sizes.nComposeHeightFactor;
+			parentWidth = scrollingContainer.getBoundingClientRect().width;
+			distanceFromTopPx = parentHeight - buttonWidth - 50;
 		}
 	}
 
 	let composeContainerCss = css``;
 	$: {
 		composeContainerCss = css`
-			height: ${composeButtonHeight}px;
-			top: ${isEditActive ? 0 : heightFromTopPx}px;
+			top: ${isEditActive ? 0 : distanceFromTopPx}px;
+			z-index: ${zIndex};
 		`;
 	}
+
+	let composeButtonWrapperCss = css`
+		margin-right: 50px;
+	`;
 </script>
 
-<div bind:this={composeToolbarWrapperRef} class="toolbar-wrapper">
-	<div bind:this={composeContainerRef} class="compose-button-container {composeContainerCss}">
+<div bind:this={composeToolbarWrapperRef} class="compose-button-absolute-wrapper">
+	<div bind:this={composeContainerRef} class="compose-button-sticky-wrapper {composeContainerCss}">
 		<!-- show compose button if not in edit mode -->
 		{#if !isEditActive}
-			<ComposeButton
-				onClickFunction={onClickCompose}
-				faIcon={composeButtonFaIcon}
-				tooltip={composeButtonTooltip}
-				buttonType={composeButtonTypes.edit.type}
-			/>
+			<div class="compose-button-wrapper {composeButtonWrapperCss}">
+				<ComposeButton
+					onClickFunction={onClickCompose}
+					faIcon={composeButtonFaIcon}
+					tooltip={composeButtonTooltip}
+					buttonType={composeButtonTypes.edit.type}
+				/>
+			</div>
 		{:else if isEditActive}
+			<div class="compose-button-wrapper {composeButtonWrapperCss}"></div>
 			<!-- show delete if function is provided -->
 			{#if onClickDelete}
 				<ComposeButton
@@ -85,24 +101,25 @@
 </div>
 
 <style>
-	.toolbar-wrapper {
+	.compose-button-absolute-wrapper {
 		position: absolute;
 		top: 0;
-		right: 0.5rem;
 		width: 100%;
 		height: 100%;
 		pointer-events: none;
 	}
 
-	.compose-button-container {
+	.compose-button-sticky-wrapper {
 		position: sticky;
 		display: flex;
 		align-items: center;
-		justify-content: right;
+		justify-content: flex-end;
 		width: 100%;
 		gap: 1rem;
 		padding-top: 1rem;
 		margin-bottom: 1rem;
-		z-index: 1000;
+	}
+
+	.compose-button-wrapper {
 	}
 </style>
